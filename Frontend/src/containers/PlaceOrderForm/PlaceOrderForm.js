d9b60ca38cf963d9908c9cf0d7c11875f509a0eb
vidurajith-darshana <vidu996@gmail.com>
2018-08-27 13:06:15 +0530

    make the layout for place order

--- Frontend/src/containers/PlaceOrderForm/PlaceOrderForm.js
+++ Frontend/src/containers/PlaceOrderForm/PlaceOrderForm.js
@@ -5,11 +5,11 @@ class PlaceOrderForm extends Component{
     render(){
         return(
 
-            <div className="row">
+            <div className="row" style={{marginLeft:'0%',marginRight:'0%'}}>
                 <div style={{backgroundColor:'green',width:'100%',height:'20px'}}>
 
                 </div>
-                <div  style={{backgroundColor:'red',width:'100%',height:'20px'}}>
+                <div className="row" style={{backgroundColor:'red',width:'100%',height:'20px',marginLeft:'0%',marginRight:'0%'}}>
                     <div style={{backgroundColor:'yellow'}} className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
                         car
                     </div>
