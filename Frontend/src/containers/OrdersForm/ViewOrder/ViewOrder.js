bf876d1bd58457003adbe8177e167e62700d1da5
vidurajith-darshana <vidu996@gmail.com>
2018-09-04 07:37:46 +0530

    finish the thogakade project successfully

--- Frontend/src/containers/OrdersForm/ViewOrder/ViewOrder.js
+++ Frontend/src/containers/OrdersForm/ViewOrder/ViewOrder.js
@@ -4,7 +4,6 @@ import {withRouter} from 'react-router-dom';
 import axios from "../../../axios/axios-order";
 import {connect} from "react-redux";
 import * as actionCreators from "../../../store/actions";
-import Button from "@material-ui/core/Button/Button";
 
 const globalCustomerImagePath="http://localhost:8080/images/customer/";
 const globalItemImagePath="http://localhost:8080/images/item/";
@@ -12,7 +11,7 @@ const globalItemImagePath="http://localhost:8080/images/item/";
 class ViewOrder extends Component{
 
     state={
-        order:""
+        order:null
     }
 
     loadOrders=(id)=>{
@@ -39,9 +38,8 @@ class ViewOrder extends Component{
     }
 
     componentDidUpdate(){
-        console.log(this.state.order.oid)
-        console.log(this.props.match.params.id)
-        if(this.state.order.oid!==this.props.match.params.id){
+
+        if(this.state.order!==null && this.state.order.oid!==parseInt(this.props.match.params.id)){
             const id=this.props.match.params.id;
             this.loadOrders(id);
         }
@@ -54,22 +52,55 @@ class ViewOrder extends Component{
     }
 
     render() {
+
+        let items=null;
+        let totalPrice=0.00;
+
+        if(this.state.order!==null){
+            items=this.state.order.orderDetailDTOList.map((order,index)=>{
+                return(
+                    <div key={index} style={{width:'90%',marginLeft:'6%',marginTop:'2%'}} className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <div className="w3-card-4">
+                            <img width="100%" height="130px" src={globalItemImagePath+""+order.item.image} alt={order.item.name}/>
+                            <div style={{display:'flex',flexDirection:'column',justifyContent:'flex-start'}} className="w3-container w3-center">
+                                <p style={{fontWeight:'bold',fontSize:'13px'}}>{"Item Name: "+order.item.name}</p>
+                                <p style={{fontSize:'12px'}}>{"Price: RS. "+order.item.price}</p>
+                                <p style={{fontSize:'12px'}}>{"Quantity :"+order.qty}</p>
+                                <p style={{fontSize:'12px'}}>{"Unit :"+order.item.unit}</p>
+                                <p style={{fontSize:'12px'}}>{"Total: Rs. "+order.totalPricePerItem}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                )
+            })
+
+            totalPrice=this.state.order.totalPrice;
+        }
+
         return (
             <div className={classes.Container}>
                 <div className="row">
                     <div className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
                         {
-                            this.state.order!==''?<div className="w3-card-4" style={{width:'90%',marginLeft:'6%',marginTop:'8%'}}>
-                                <img width="100%" height="130px" src={globalCustomerImagePath+""+this.state.order.customerDTO.image} alt={this.state.order.customerDTO.name}/>
-                                <div style={{display:'flex',flexDirection:'column',justifyContent:'flex-start'}} className="w3-container w3-center">
-                                    <p style={{fontWeight:'bold',fontSize:'13px'}}>{this.state.order.customerDTO.name}</p>
-                                    <p style={{fontSize:'12px'}}>{this.state.order.customerDTO.address}</p>
+                            this.state.order!==null?<div style={{display:'flex',flexDirection:'column',justifyContent:'flex-start'}}>
+                                <div className="w3-card-4" style={{width:'90%',marginLeft:'6%',marginTop:'8%'}}>
+                                    <img width="100%" height="130px" src={globalCustomerImagePath+""+this.state.order.customerDTO.image} alt={this.state.order.customerDTO.name}/>
+                                    <div style={{display:'flex',flexDirection:'column',justifyContent:'flex-start'}} className="w3-container w3-center">
+                                        <p style={{fontWeight:'bold',fontSize:'13px'}}>{this.state.order.customerDTO.name}</p>
+                                        <p style={{fontSize:'12px'}}>{this.state.order.customerDTO.address}</p>
+                                    </div>
+                                </div>
+                                <div className="w3-card-4" style={{width:'90%',height:'40px',marginLeft:'6%',marginTop:'8%',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+                                    Total Price:{"Rs. "+totalPrice}
                                 </div>
                             </div>:null
                         }
                     </div>
                     <div className="col-12 col-sm-9 col-md-9 col-lg-9 col-xl-9">
-
+                        <div className="row">
+                            {items}
+                        </div>
                     </div>
                 </div>
             </div>
